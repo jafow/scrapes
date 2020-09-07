3aefb5bf132ec71892ac7f6e30362f4368ee17f3
jafow <jared.a.fowler@gmail.com>
2020-09-07 10:55:44 -0700

    checkpoint: peel out the name

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{fs, str};
 use std::path::Path;
 
 use select::document::Document;
-use select::predicate::{Predicate, Attr, Class, Name};
+use select::predicate::{Predicate, Attr, Child, Class, Element, Name};
 
 fn main() -> std::io::Result<()> {
     let data_path = Path::new("data/complete-list.html");
@@ -10,8 +10,32 @@ fn main() -> std::io::Result<()> {
     let docstr = str::from_utf8(&f).unwrap();
 
     let doc = Document::from(docstr);
-    for node in doc.find(Class("results")) {
-        println!("node {:?}", node);
+    for node in doc.find(Name("article")) {
+        let article = Document::from(node.html().as_str());
+
+        for node in article.find(Name("h2")).take(2) {
+            let county_and_auditor_name = node;
+            dbg!(county_and_auditor_name.text());
+            // county
+            //
+            // address 1
+            //
+            // address 2?
+            //
+            // city
+            // zip
+            // phone
+            //
+            // county website url
+            //
+            // email
+            //
+            // fax
+            //
+            // hours
+            //
+            // social
+        }
     }
     Ok(())
 }
