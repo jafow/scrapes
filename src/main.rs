dedb4947990d6804a49eae71ed68d7b3f9ca782e
jafow <jared.a.fowler@gmail.com>
2020-09-09 11:07:05 -0700

    Fixup output for csv;
    add headers
    open a file and write piece by piece
    change order of output to match sheet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 use std::{fs, str};
+use std::fs::OpenOptions;
+use std::io::Write;
 
 use select::document::Document;
 use select::predicate::{Class, Name};
@@ -41,26 +43,36 @@ fn trim_address_test() {
     assert_eq!(
         Some(Address { street: "400 Public Square Ste 5".to_string(), city: "Greenfield".to_string(), state: "IA".to_string(), zip_code: "50849".to_string()}),
         to_address(String::from("\n\t\t\t\t\t\t\t\t\t\t400 Public Square Ste 5\u{a0}\n\t\t\t\t\t\t\tGreenfield\u{a0}IA\u{a0}50849\n\t\t\t\t\t")));
+
+    assert_eq!(
+        Some(Address { street: "400 Public Square, Ste 5".to_string(), city: "Greenfield".to_string(), state: "IA".to_string(), zip_code: "50849".to_string()}),
+        to_address(String::from("\n\t\t\t\t\t\t\t\t\t\t400 Public Square, Ste 5\u{a0}\n\t\t\t\t\t\t\tGreenfield\u{a0}IA\u{a0}50849\n\t\t\t\t\t")));
 }
 
 fn main() -> std::io::Result<()> {
+    // input
     let data_path = Path::new("data/complete-list.html");
     let f = fs::read(data_path)?;
-    let docstr = str::from_utf8(&f).unwrap();
 
+    // output
+    let output_data = Path::new("data/output.csv");
+    let mut output = OpenOptions::new().create(true).append(true).open(output_data)?;
+    let headers = "state,county,position,name,address,address2,email,fax,phone,website_of_county,source_url,maps_url";
+    writeln!(&mut output, "{}", headers)?;
+
+    // scrape
+    let docstr = str::from_utf8(&f).unwrap();
     let doc = Document::from(docstr);
+
     for node in doc.find(Name("article")) {
         let article = Document::from(node.html().as_str());
 
         for (h2, table) in article
             .find(Name("h2"))
             .zip(article.find(Class("results")))
-            .take(1)
         {
             let mut data: String = String::new();
             let (county_name, auditor_name) = name_data(&h2.text());
-            data.push_str(&format!(",{}", county_name));
-            data.push_str(&format!(",{}", auditor_name));
 
             // get data elements holding contact and location info
             let td_selections = table
@@ -69,36 +81,11 @@ fn main() -> std::io::Result<()> {
                 .iter()
                 .collect::<Vec<_>>();
             let phone = td_selections[0].children().next().unwrap().text();
-            data.push_str(&format!(",{}", phone));
-
             let fax = td_selections[1].text();
-            data.push_str(&format!(",{}", fax));
-
             let email = td_selections[2].children().next().unwrap().text();
-            data.push_str(&format!(",{}", email));
-
             let website = td_selections[3].children().next().unwrap().text();
-            data.push_str(&format!(",{}", website));
-
-            // mailing
             let mailing_address = to_address(td_selections[5].text()).unwrap();
-            data.push_str(&format!(",{}", mailing_address.street));
-            data.push_str(&format!(",{}", mailing_address.city));
-            data.push_str(&format!(",{}", mailing_address.state));
-            data.push_str(&format!(",{}", mailing_address.zip_code));
-
-            // hours
-            let hours = td_selections[4].text();
-            data.push_str(&format!(",{}", hours));
-
-            // physical address
             let physical_address = to_address(td_selections[6].text()).unwrap();
-            data.push_str(&format!(",{}", physical_address.street));
-            data.push_str(&format!(",{}", physical_address.city));
-            data.push_str(&format!(",{}", physical_address.state));
-            data.push_str(&format!(",{}", physical_address.zip_code));
-
-            // google maps url
             let map_url = td_selections[7]
                 .find(Name("a"))
                 .take(1)
@@ -106,10 +93,29 @@ fn main() -> std::io::Result<()> {
                 .unwrap()
                 .attr("href")
                 .unwrap();
+
+            data.push_str(&format!("{}", "iowa"));
+            data.push_str(&format!(",{}", county_name));
+            data.push_str(&format!(",{}", "auditor"));
+            data.push_str(&format!(",{}", auditor_name));
+            // mailing
+            data.push_str(&format!(",\"{}{}{}{}\"", mailing_address.street, mailing_address.city,  mailing_address.state, mailing_address.zip_code));
+            // physical address
+            data.push_str(&format!(",\"{}{}{}{}\"", physical_address.street, physical_address.city,  physical_address.state, physical_address.zip_code));
+
+            data.push_str(&format!(",{}", email));
+
+            data.push_str(&format!(",{}", fax));
+
+            data.push_str(&format!(",{}", phone));
+
+            data.push_str(&format!(",{}", website));
+            data.push_str(&format!(",{}", "https://sos.iowa.gov/elections/auditors/auditor.asp?CountyID=00"));
+            // google maps url
             data.push_str(&format!(",{}", String::from(map_url)));
 
-            // write it to the output
-            println!("{}", data);
+            // write it to the output file
+            writeln!(&mut output, "{}", data)?;
         }
     }
     Ok(())
